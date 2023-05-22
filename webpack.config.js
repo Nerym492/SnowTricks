03ff2b3f3cb07113b407b775b51cf0bd132fa570
Florian Pohu <florianpohu49@gmail.com>
2023-05-22 12:33:40 +0200

    Webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,43 @@
 const Encore = require('@symfony/webpack-encore');
+const StylelintPlugin = require('stylelint-webpack-plugin');
 
 Encore
+  .enableSingleRuntimeChunk()
   .setOutputPath('public/build/')
   .setPublicPath('/build')
   .addEntry('app', './assets/js/app.js')
-  .addStyleEntry('app', './assets/sass/app.scss')
+  .addStyleEntry('styles', './assets/sass/app.scss')
   .enableSassLoader()
   .enablePostCssLoader()
   .cleanupOutputBeforeBuild()
   .enableSourceMaps(!Encore.isProduction())
-  .enableVersioning(Encore.isProduction());
+  .enableVersioning(Encore.isProduction())
 
-module.exports = Encore.getWebpackConfig();
+const webpackConfig = Encore.getWebpackConfig();
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  plugins: [
+    new StylelintPlugin({
+      configFile: '.stylelintrc.json',
+      files: '**/*.css', // Chemin vers les fichiers CSS générés par Sass
+    }),
+  ],
+  ...webpackConfig,
+};
+
+
+
+
+
+
