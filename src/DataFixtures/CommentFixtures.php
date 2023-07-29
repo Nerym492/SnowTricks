e953d7e5721800d61ae9cf11d01fc46cc462ae4a
Florian Pohu <florianpohu49@gmail.com>
2023-07-29 10:37:00 +0200

    Change CommentFixtures

--- src/DataFixtures/CommentFixtures.php
+++ src/DataFixtures/CommentFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Comment;
+use App\Entity\Trick;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
@@ -20,6 +21,7 @@ class CommentFixtures extends Fixture implements DependentFixtureInterface
     {
         return [
             UserFixtures::class,
+            TrickFixtures::class,
         ];
     }
 
@@ -32,10 +34,12 @@ class CommentFixtures extends Fixture implements DependentFixtureInterface
     public function load(ObjectManager $manager)
     {
         $user = $manager->getRepository(User::class)->findOneBy(['pseudo' => 'Testuser1234']);
+        $trick = $manager->getRepository(Trick::class)->findOneBy(['name' => 'Indy']);
         $comment = new Comment();
         $comment->setUser($user);
         $comment->setContent('Mon tout premier commentaire sur ce site !!!');
         $comment->setCreationDate(new \DateTime());
+        $comment->setTrick($trick);
 
         $manager->persist($comment);
         $manager->flush();
